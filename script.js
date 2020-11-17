5b7126ba5c0f763358758b64c70f379bead91002
KLIPFEL <jean-pascal.klipfel@grandest.fr>
2020-11-17 10:43:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,11 +30,13 @@ function getColor(d) {
   var date = new Date();
   var date_fv_SIM = new Date(d);
   var date1m = new Date(date.setMonth(date.getMonth()+2));
+  var nodata = checkNull(d);
   return date_fv_SIM > date1m ? '#83e60b' :
          date < date_fv_SIM < date1m ? '#e6aa0b' :
+         nodata == '--' ? '#e6220b':
       //   date1m > date_fv_SIM < date ? '#e99813' :
       //   date_fv_SIM < date ? '#eb1f3e':
-                              '#e99813';
+                              '#8f9695';
 }
 
 //filtering network
