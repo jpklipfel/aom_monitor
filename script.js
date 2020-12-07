0be053c92b30cb17ba1b5da3745bf491a330d9c8
KLIPFEL <jean-pascal.klipfel@grandest.fr>
2020-12-07 16:33:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ $.getJSON("https://www.geograndest.fr/geoserver/region-grand-est/ows?service=WFS
 // Any values not listed in the ranges below displays as the last color
 function getColor(d) {
   var date = new Date();
-  var d_worked = d.substr(9,2);
+  var d_worked = d.substr(9, 2), d.substr(1, 4);
   var date_fv_SIM = new Date(d_worked);
   console.log(d);
   console.log(date);
